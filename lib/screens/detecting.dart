3fda01010dceef78be72d1b0a516fdefc5776a06
veronica2550 <minkyoung2550@gmail.com>
2024-05-23 00:32:19 +0900

    feat: 프론트 camera이미지 xfile로 변환 후 uint8list로 처리해서 서버로 연결

--- lib/screens/detecting.dart
+++ lib/screens/detecting.dart
@@ -9,6 +9,7 @@ import 'dart:async';
 
 import 'package:sayosayo_client/api/SayoSayoApi.dart';
 
+
 class Detecting extends StatefulWidget {
   const Detecting({
     Key? key,
@@ -22,7 +23,11 @@ class Detecting extends StatefulWidget {
 }
 
 class _DetectingState extends State<Detecting> {
-  late CameraController _controller;
+  late CameraController _cameraController;
+  bool _isSending = false;
+  String? _serverResponse;
+  Timer? _timer;
+
   late Future<void> _initializeControllerFuture;
   FlutterTts flutterTts = FlutterTts();
   String language = "ko-KR";
@@ -33,38 +38,83 @@ class _DetectingState extends State<Detecting> {
   double rate = 0.5;
   String serverResponse = '';
 
-  Timer? _frameTimer;
-  int frameInterval = 1000; // 1000 milliseconds = 1 second
 
+  @override
   @override
   void initState() {
     super.initState();
-    initTts();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.max,
-      enableAudio: false,
+    _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    final camera = cameras.first;
+
+    _cameraController = CameraController(
+      camera,
+      ResolutionPreset.medium,
     );
-    _initializeControllerFuture = _controller.initialize().then((_) {
-      if (!mounted) {
-        return;
+
+    await _cameraController.initialize();
+    setState(() {});
+
+    // Start the timer to capture and send image every second
+    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      if (!_isSending) {
+        _captureAndSendImage();
       }
-      _controller.startImageStream((CameraImage image) {
-        if (_frameTimer == null || !_frameTimer!.isActive) {
-          _frameTimer = Timer(Duration(milliseconds: frameInterval), () {
-            sendImageToServer(image);
-          });
-        }
-      });
     });
+  }
+
+  Future<void> _captureAndSendImage() async {
+    setState(() {
+      _isSending = true;
+      _serverResponse = null;
+    });
+
+    try {
+      XFile? capturedImage = await _cameraController.takePicture();
+
+      if (capturedImage != null) {
+        Uint8List imageBytes = await capturedImage.readAsBytes();
+        print("Image bytes length: ${imageBytes.length}");
+
+        final response = await http.post(
+          Uri.parse('${API.hostConnect}/stream'),
+          headers: {
+            'Content-Type': 'application/octet-stream',
+          },
+          body: imageBytes,
+        );
+
+        if (response.statusCode == 200) {
+          setState(() {
+          Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+          serverResponse = decodedResponse['message'];
+          });
+          print("Response from server: ${response.body}");
+        } else {
+          print("Failed to send image to server: ${response.statusCode}");
+          print("Response body: ${response.body}");
+        }
+      }
+    } catch (e) {
+      print("Error capturing or sending image: $e");
+    } finally {
+      setState(() {
+        _isSending = false;
+      });
+    }
   }
 
   @override
   void dispose() {
-    _frameTimer?.cancel();
-    _controller.dispose();
+    _cameraController.dispose();
+    _timer?.cancel();
     super.dispose();
   }
+
+
 
   initTts() async {
     await initTtsIosOnly(); // iOS 설정
@@ -100,64 +150,24 @@ class _DetectingState extends State<Detecting> {
   return base64Encode(data);
 }
 
-// 이미지를 Base64로 인코딩하여 서버로 전송하는 함수
-Future<void> sendImageToServer(CameraImage image) async {
-  final bytes = concatenatePlanes(image.planes);
-  final String base64Image = await base64String(bytes);
-
-  try {
-    final response = await http.post(
-      Uri.parse('${API.hostConnect}/stream'),
-      headers: {
-        'Content-Type': 'application/json', // 이미지를 JSON 형식으로 전송
-      },
-      body: jsonEncode({'image': base64Image}), // 이미지를 JSON으로 인코딩하여 전송
-    );
-
-    // 이하 응답 처리 코드...
-  } catch (e) {
-    print("Error sending image to server: $e");
-  }
-}
-
-
-  Uint8List concatenatePlanes(List<Plane> planes) {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (Plane plane in planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-    Uint8List result = allBytes.done().buffer.asUint8List();
-    print("Concatenated bytes length: ${result.length}"); // 추가된 로그
-    return result;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Column(
         children: [
-          Expanded(
-            child: FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  // 미리보기
-                  return CameraPreview(_controller);
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            ),
-          ),
-          Center(
-            child: Text(
-              serverResponse,
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+          if (_cameraController.value.isInitialized)
+            CameraPreview(_cameraController),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_isSending) CircularProgressIndicator(),
+              if (_serverResponse != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text('Server response: $_serverResponse'),
+                ),
+              SizedBox(height: 20),
+            ],
           ),
         ],
       ),
@@ -185,3 +195,59 @@ Future<void> sendImageToServer(CameraImage image) async {
     );
   }
 }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: FutureBuilder<void>(
+//               future: _initializeControllerFuture,
+//               builder: (context, snapshot) {
+//                 if (snapshot.connectionState == ConnectionState.done) {
+//                   // 미리보기
+//                   return CameraPreview(_controller);
+//                 } else {
+//                   return const Center(child: CircularProgressIndicator());
+//                 }
+//               },
+//             ),
+//           ),
+//           Center(
+//             child: Text(
+//               serverResponse,
+//               style: TextStyle(
+//                 fontSize: 24,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//       bottomNavigationBar: GestureDetector(
+//         behavior: HitTestBehavior.translucent,
+//         onTap: () {
+//           _speak(serverResponse);
+//         },
+//         onLongPress: () async {
+//           Navigator.pushNamedAndRemoveUntil(
+//               context, '/onboarding', (route) => false);
+//         },
+//         child: Container(
+//           color: Colors.amber,
+//           width: double.maxFinite,
+//           height: 150,
+//           child: Center(
+//             child: Text(
+//               '음성',
+//               style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
